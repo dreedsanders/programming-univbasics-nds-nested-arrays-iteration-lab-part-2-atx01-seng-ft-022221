65f7c85fdbdb43fa5ff69af79c321358de7ef0d5
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-14 19:56:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_min_in_nested_arrays(src)
     daily_low_element = () 
       element_index = 0
     while element_index< src[row_index].count do
-          src[row_index][element_index] = daily_low_element
+          src[row_index][element_index] 
           if src[row_index][element_index] < daily_low_element
         daily_low_element = src[row_index][element_index]
       end
