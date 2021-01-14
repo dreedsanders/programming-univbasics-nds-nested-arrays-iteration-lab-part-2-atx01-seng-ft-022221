6b50c3a9494d540cc8a7dd2d10205db964acc67d
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-14 20:09:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,7 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do 
       element_index = 0
       daily_low_element = 100 
+      # i dont know what to equal to the daily low element. 3 of my weeks would be off if I waited one level and set it equal to the src[row_index][element_index] and i would get a full error putting that in this level. but thankfully it passed i guess. help? 
     while element_index< src[row_index].count do
        
           if src[row_index][element_index] < daily_low_element
