8560873715f6f9add3feb5da9094a4a74220a039
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-14 20:00:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,11 @@ def find_min_in_nested_arrays(src)
       element_index = 0
     while element_index< src[row_index].count do
       daily_low_element = src[row_index][element_index] 
-          if src[row_index][element_index] <= daily_low_element
+          if src[row_index][element_index] < daily_low_element
         daily_low_element = src[row_index][element_index]
       end
       element_index += 1
+      daily_low_element = src{row_index][element_index] += 1 
     end
     row_index += 1 
      low_temp_each_day<< daily_low_element
